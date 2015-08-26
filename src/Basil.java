f19b69c3749dbdd8a893cf9272483b96ec6ff1fa
italofini <italo.fini@techhigh.us>
2015-08-25 23:58:45 -0400

    MCM

---
+++ src/Basil.java
@@ -0,0 +1,12 @@
+public class Basil {
+	public static void main(String[] args) {
+		System.out.println(Math.abs(-26.7)); // Abousolute value. ex: 26.7 = 26.7 
+		System.out.println(Math.ceil(7.4));// Ceiling round up. ex: 7.4 = 8
+		System.out.println(Math.floor(7.4));// Floor round down. ex: 7.4 = 7
+		System.out.println(Math.max(8.6,5.2));// max =8.6
+		System.out.println(Math.min(8.6,5.2)); //min =5
+		System.out.println(Math.pow(8,6)); //ex: 8^6
+		System.out.println(Math.sqrt(9)); //Square root
+	}
+
+}
